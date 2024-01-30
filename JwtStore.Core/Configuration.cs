22dcaf11434fb81182dc5647edd766bc05bec601
dizzysch <victorwilbert@gmail.com>
2024-01-30 11:34:34 -0300

    feat: add infra configurations and services

--- JwtStore.Core/Configuration.cs
+++ JwtStore.Core/Configuration.cs
@@ -3,12 +3,24 @@
     public static class Configuration
     {
         public static SecretsConfiguration Secrets { get; set; } = new();
-
         public static DatabaseConfiguration Database { get; set; } = new();
-        
+        public static EmailConfiguration Email { get; set; } = new();
+        public static SendGridConfiguration SendGrid { get; set; } = new();
+
         public class DatabaseConfiguration
         {
             public string ConnectionString { get; set; } = string.Empty;
+        }
+
+        public class EmailConfiguration
+        {
+            public string DefaultFromEmail { get; set; } = "teste@victorschlindwein.com.br";
+            public string DefaultFromName { get; set; } = "teste@victorschlindwein.com.br";
+        }
+
+        public class SendGridConfiguration
+        {
+            public string ApiKey { get; set; } = string.Empty;
         }
 
         public class SecretsConfiguration
--- JwtStore.Infra/Contexts/AccountContext/UseCases/Create/Service.cs
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Create/Service.cs
@@ -1,13 +1,22 @@
-﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+﻿using JwtStore.Core;
+using JwtStore.Core.Contexts.AccountContext.Entities;
 using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Create
 {
     public class Service : IService
     {
-        public Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
+        public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var client = new SendGridClient(Configuration.SendGrid.ApiKey);
+            var from = new EmailAddress(Configuration.Email.DefaultFromEmail, Configuration.Email.DefaultFromName);
+            const string subject = "Verifique sua conta";
+            var to = new EmailAddress(user.Email, user.Name);
+            var content = $"Código {user.Email.Verification.Code}";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            await client.SendEmailAsync(msg, cancellationToken);
         }
     }
 }
