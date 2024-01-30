92f6b9a77f0f4203f60072e577ca6b8d769396b4
dizzysch <victorwilbert@gmail.com>
2024-01-30 17:37:50 -0300

    feat: implements authentication

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -1,5 +1,4 @@
-﻿using JwtStore.Core.Contexts.AccountContext.UseCases.Create;
-using MediatR;
+﻿using MediatR;
 
 namespace JwtStore.Api.Extensions
 {
@@ -18,21 +17,51 @@ namespace JwtStore.Api.Extensions
                 JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>();
 
             #endregion
+
+            #region Authenticate
+
+            builder.Services.AddTransient<
+                JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts.IRepository,
+                JwtStore.Infra.Contexts.AccountContext.UseCases.Authenticate.Repository>();
+
+            #endregion
         }
 
         public static void MapAccountEndpoints(this WebApplication app)
         {
             #region Create
 
-            app.MapPost("api/v1/users", async (Request request,
+            app.MapPost("api/v1/users", async (Core.Contexts.AccountContext.UseCases.Create.Request request,
                 IRequestHandler<
-                    Request, 
-                    Response> handler) =>
+                    Core.Contexts.AccountContext.UseCases.Create.Request,
+                    Core.Contexts.AccountContext.UseCases.Create.Response> handler) =>
             {
                 var result = await handler.Handle(request, new CancellationToken());
                 return result.IsSuccess
                     ? Results.Created($"api/v1/users{result.Data?.Id}", result)
                     : Results.Json(result, statusCode: result.Status);
+            });
+
+            #endregion
+
+            #region Authenticate
+
+
+            app.MapPost("api/v1/auth",
+                async (Core.Contexts.AccountContext.UseCases.Authenticate.Request request,
+                IRequestHandler<
+                    Core.Contexts.AccountContext.UseCases.Authenticate.Request,
+                    Core.Contexts.AccountContext.UseCases.Authenticate.Response> handler) =>
+            {
+                var result = await handler.Handle(request, new CancellationToken());
+                if (!result.IsSuccess)
+                    return Results.Json(result, statusCode: result.Status);
+
+                if (result.Data is null)
+                    return Results.Json(result, statusCode: 500);
+
+                result.Data.Token = JwtExtension.Generate(result.Data);
+                return Results.Ok(result);
             });
 
             #endregion
